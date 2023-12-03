204deaf8873faa4a5a30e1a7a24567e8be4ffd3b
Olga Boris <olya_boris97@mail.ru>
2023-12-04 01:59:35 +0300

    update

--- js/header.js
+++ js/header.js
@@ -50,3 +50,18 @@ if (iconMenu) {
         menuBody.classList.toggle('_active')
     })
 }
+
+
+function openInfo(evt, info) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(info).style.display = "block";
+    evt.currentTarget.className += " active";
+}
