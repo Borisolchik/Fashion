2e6c55260c8f59a5c9db02caf22d79fa4905db70
Olga Boris <olya_boris97@mail.ru>
2023-12-04 09:36:29 +0300

    slider edits on the index page

--- js/swiperBrand.js
+++ js/swiperBrand.js
@@ -19,38 +19,15 @@ new Swiper('.image-slider1', {
     slidesPerGroup: 1,
 });
 
-new Swiper('.image-slider2', {
-    speed: 700,
-    spaceBetween: 22,
-    slidesPerView: 3,
-    navigation: {
-        nextEl: '.image-slider2 .swiper-button-next',
-        prevEl: '.image-slider2 .swiper-button-prev',
-    },
-    slideToClickedSlide: true,
-    keyboard: {
-        enabled: true
-    }, 
-    // autoHeight: true,
-    // loop: true,
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    // },
-    // slidesPerView: 'auto',
-    slidesPerGroup: 1,
-});
-
-
 $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    // speed: 1000,
-    prevArrow: '<div class="slick-prev"><img src="../assets/images/icons/arr-1.svg" alt=""></div>',
-    nextArrow: '<div class="slick-next"><img src="../assets/images/icons/arr-2.svg" alt=""></div>',
+    speed: 1000,
+    prevArrow: '<div class="slick-prev"><img src="assets/images/icons/arr-1.svg" alt=""></div>',
+    nextArrow: '<div class="slick-next"><img src="assets/images/icons/arr-2.svg" alt=""></div>',
     responsive: [
         {
             breakpoint: 900,
