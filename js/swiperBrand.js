f17479bba58c7a94fcd542243297d2601ed0a7de
Olga Boris <olya_boris97@mail.ru>
2023-12-04 11:12:46 +0300

    slider-discont edits on the index page

--- js/swiperBrand.js
+++ js/swiperBrand.js
@@ -1,30 +1,30 @@
-new Swiper('.image-slider1', {
-    speed: 700,
-    spaceBetween: 30,
-    navigation: {
-        nextEl: '.image-slider1 .swiper-button-next',
-        prevEl: '.image-slider1 .swiper-button-prev',
-    },
-    slideToClickedSlide: true,
-    keyboard: {
-        enabled: true
-    }, 
-    // autoHeight: true,
-    // loop: true,
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    // },
-    slidesPerView: 'auto',
-    slidesPerGroup: 1,
-});
-
 $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    prevArrow: '<div class="slick-prev"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M0.0143415 8.60912L15.0267 16.8824L15.0019 0.290927L0.0143415 8.60912Z" fill="#D2AD95"/>\n' +
+        '</svg>\n</div>',
+    nextArrow: '<div class="slick-next"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M15.014 8.60912L0.00157572 16.8824L0.0264159 0.290927L15.014 8.60912Z" fill="#D2AD95"/>\n' +
+        '</svg>\n</div>',
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 1
+            }
+        },
+    ]
+});
+
+$('.multiple-items2').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
     speed: 1000,
     prevArrow: '<div class="slick-prev"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
         '<path d="M0.0143415 8.60912L15.0267 16.8824L15.0019 0.290927L0.0143415 8.60912Z" fill="#D2AD95"/>\n' +
